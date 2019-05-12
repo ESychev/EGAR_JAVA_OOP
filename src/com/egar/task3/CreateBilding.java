f41b6f0cf2ff81fb74823d7393c4ce9bd742ab2d
ESychev <ESychev@bitbucket.org>
2019-05-12 19:28:33 +0300

    Документирование  кода согласно стандартам

--- src/com/egar/task3/CreateBilding.java
+++ src/com/egar/task3/CreateBilding.java
@@ -22,7 +22,14 @@ public class CreateBilding implements FactoryCreateBilding {
 	 */
 	@Override
 	public Building create() {
-		System.out.println("What are we going to build? House (1) or Bank(2).");
+		System.out.println(" ƒомашн€€ работа. ");
+		System.out.println("1 ќписать иерархию классов дл€ приложени€ дл€ темы —троение\r\n"
+				+ "2 »ерархи€ должна включать в себ€ минимум один базовый абстрактный класс и два наследника.\r\n"
+				+ "3 ¬се свойства классов об€заны иметь модификаторы видимости private и соответствующие им методы-мутаторы.\r\n"
+				+ "4 ¬ базовом классе должно быть поле типа enum \r\n"
+				+ "5 ќписать конструкторы (по умолчанию и со всеми параметрами), переопределить методы toString, equals.\r\n"
+				+ "6 —оздать класс-фабрику дл€ создани€ объектов иерархии.\r\n ");
+		System.out.println("What are we going to build? House (1) or Bank(2).\r\n");
 
 		if (st.nextInt() == 1) {
 			System.out.println("Enter City");
